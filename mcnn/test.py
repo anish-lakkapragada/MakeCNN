c312775bbedf164137bbd68601ed37d637858484
Anish <68448458+anish-lakkapragada@users.noreply.github.com>
2021-03-21 00:01:16 -0700

    Delete mcnn directory

--- mcnn/test.py
+++
@@ -1,6 +0,0 @@
-# just to test the API
-from Class_project import AutoWork
-
-aw = AutoWork("/Users/anish/Downloads/xray_dataset_covid19")
-aw.train(epochs = 1)
-aw.predict("/Users/anish/Downloads/xray_dataset_covid19/Testing/normal/NORMAL2-IM-0120-0001.jpeg")
